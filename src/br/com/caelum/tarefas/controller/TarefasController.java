0a24ef283697d6b14bef27996f2b4d92b82d34e6
danielpfernandes <daniel.paivafernandes@gmail.com>
2018-06-02 18:45:07 -0300

    Exercícios do capítulo 11

--- src/br/com/caelum/tarefas/controller/TarefasController.java
+++ src/br/com/caelum/tarefas/controller/TarefasController.java
@@ -2,12 +2,14 @@ package br.com.caelum.tarefas.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.caelum.tarefas.dao.JdbcTarefaDao;
@@ -37,5 +39,33 @@ public class TarefasController {
 		JdbcTarefaDao dao = new JdbcTarefaDao();
 		model.addAttribute("tarefas", dao.lista());
 		return "tarefa/lista";
+	}
+	
+	@RequestMapping("removeTarefa")
+	public String remove(Tarefa tarefa) {
+		JdbcTarefaDao dao = new JdbcTarefaDao();
+		dao.remove(tarefa);
+		return "redirect:listaTarefas";
+	}
+	
+	@RequestMapping("mostraTarefa")
+	public String mostra(Long id,Model model) {
+		JdbcTarefaDao dao = new JdbcTarefaDao();
+		model.addAttribute("tarefa", dao.buscaPorId(id));
+		return "tarefa/mostra";
+	}
+	
+	@RequestMapping("alteraTarefa")
+	public String altera(Tarefa tarefa) {
+		JdbcTarefaDao dao = new JdbcTarefaDao();
+		dao.altera(tarefa);
+		return "redirect:listaTarefas";
+	}
+	
+	@ResponseBody
+	@RequestMapping("finalizaTarefa")
+	public void finaliza(Long id) {
+		JdbcTarefaDao dao = new JdbcTarefaDao();
+		dao.finaliza(id);
 	}
 }
